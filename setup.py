c093a6fda145f49f6c0a5a411e6404a39733dff5
Anders <55411265+Anders-Hopkins@users.noreply.github.com>
2021-04-27 17:42:49 -0500

    updated req versions in setup.py

--- setup.py
+++ setup.py
@@ -34,13 +34,14 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as readme_file:
 
 requirements = [
     
-    'pyproj==2.6.1',
-    'proj==6.2.1',
+    
     'geojson==2.5.0',
     'numpy==1.20.2',
     'pyflwdir',
     'requests==2.25.1',
     'shapely==1.7.1',
+    'pyproj=<3.0.0',
+    'proj',
     'rasterio==1.2.2'
 ]
 
